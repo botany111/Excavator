49f93e16413306c43843f81a6f5c6d0fc225e98e
botany111 <55132891+botany111@users.noreply.github.com>
2024-01-20 15:06:59 +0800

    上迴旋體 旋轉

--- Assets/Scripts/TopRotation.cs
+++ Assets/Scripts/TopRotation.cs
@@ -1,18 +1,33 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TopRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 rotateVector;
 
-    // Update is called once per frame
-    void Update()
+    [Header("旋轉速度")]
+    public float rotationSpeed = 5f;
+
+    [Header("上迴旋體物件")]
+    public GameObject topRotation;
+
+    private void Update()
     {
-        
+        _TopRotation();
+    }
+    public void _TopRotation()
+    {
+        // 计算旋转角度
+        float rotationAmount = rotateVector.x * rotationSpeed * Time.deltaTime;
+
+        // 应用旋转
+        topRotation.transform.Rotate(Vector3.up, rotationAmount);
+    }
+    public void Rotation(InputAction.CallbackContext ctx)
+    {
+        rotateVector = ctx.ReadValue<Vector2>();
+        Debug.Log(rotateVector);
     }
 }
